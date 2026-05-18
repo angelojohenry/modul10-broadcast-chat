bff54f17a5cfe9e0a4ca3103afc943102029c276
angelojohenry <johenry.angelo@gmail.com>
2026-05-18 11:32:13 +0700

    Experiment 2.1: Original code, and how it run

---
+++ README.md
@@ -0,0 +1,6 @@
+## 2.1 Original code, and how it run
+Program dijalankan dengan satu server dan beberapa client. Ketika salah satu client mengetik pesan, pesan tersebut dikirim ke server melalui websocket. Server kemudian membroadcast pesan itu ke client lain. Karena itu, pesan dari satu client dapat terlihat di client lainnya.
+![img.png](screenshots/img.png)
+![img_2.png](screenshots/img_2.png)
+![img_3.png](screenshots/img_3.png)
+![img_1.png](screenshots/img_1.png)
---
+++ src/bin/client.rs
@@ -0,0 +1,61 @@
+use futures_util::SinkExt;
+use futures_util::stream::StreamExt;
+use http::Uri;
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio_websockets::{ClientBuilder, Message};
+
+#[tokio::main]
+async fn main() -> Result<(), tokio_websockets::Error> {
+    let (mut ws_stream, _) =
+        ClientBuilder::from_uri(Uri::from_static("ws://127.0.0.1:2000"))
+            .connect()
+            .await?;
+
+    let stdin = tokio::io::stdin();
+    let mut stdin = BufReader::new(stdin).lines();
+
+
+    loop {
+        tokio::select! {
+            input = stdin.next_line() => {
+                match input {
+                    Ok(Some(line)) => {
+                        ws_stream.send(Message::text(line)).await?;
+                    }
+
+                    Ok(None) => {
+                        break;
+                    }
+
+                    Err(e) => {
+                        eprintln!("Error reading stdin: {e}");
+                        break;
+                    }
+                }
+            }
+
+            msg = ws_stream.next() => {
+                match msg {
+                    Some(Ok(msg)) if msg.is_text() => {
+                        println!("{}", msg.as_text().unwrap());
+                    }
+
+                    Some(Ok(_)) => {}
+
+                    Some(Err(e)) => {
+                        eprintln!("Error receiving message: {e}");
+                        return Err(e);
+                    }
+
+                    None => {
+                        println!("Server disconnected");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+
+}
